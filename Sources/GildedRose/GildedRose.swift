1d5300d605b39ad883c037e1a0e4f60ec68cb7ad
MrNasch <nicolaspaschal@hotmail.com>
2020-04-15 21:35:42 +0200

    added switch case on item name to refactor the updateQuality func

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -14,57 +14,75 @@ public class GildedRose {
     
     // update Quality
     public func updateQuality() {
-        for i in 0..<items.count {
+        for item in items {
             
-            
-            
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality -= 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality += 1
-                    
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality += 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality += 1
-                            }
-                        }
-                    }
-                }
+            // swich on all item name
+            switch item.name {
+            case "+5 Dexterity Vest":
+                print("quality -1")
+            case "Aged Brie":
+                print("quality +1")
+            case "Elixir of the Mongoose":
+                print("quality -1")
+            case "Sulfuras, Hand of Ragnaros":
+                print("nothing change")
+            case "Backstage passes to a TAFKAL80ETC concert":
+                print("quality +1 if more than 10 days +2 between 10 and 6 and + 3 between 5 and 0 then 0 after the show")
+            case "Conjured Mana Cake":
+                print("quality -2  and -4 when after the date")
+            default:
+                break
             }
             
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn -= 1
-            }
             
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality -= 1
-                            }
-                        }
-                    } else {
-                        items[i].quality -= items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1
-                    }
-                }
-            }
-        }
+            
+//            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+//                if (items[i].quality > 0) {
+//                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+//                        items[i].quality -= 1
+//                    }
+//                }
+//            } else {
+//                if (items[i].quality < 50) {
+//                    items[i].quality += 1
+//
+//                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+//                        if (items[i].sellIn < 11) {
+//                            if (items[i].quality < 50) {
+//                                items[i].quality += 1
+//                            }
+//                        }
+//
+//                        if (items[i].sellIn < 6) {
+//                            if (items[i].quality < 50) {
+//                                items[i].quality += 1
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+//                items[i].sellIn -= 1
+//            }
+//
+//            if (items[i].sellIn < 0) {
+//                if (items[i].name != "Aged Brie") {
+//                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+//                        if (items[i].quality > 0) {
+//                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+//                                items[i].quality -= 1
+//                            }
+//                        }
+//                    } else {
+//                        items[i].quality -= items[i].quality
+//                    }
+//                } else {
+//                    if (items[i].quality < 50) {
+//                        items[i].quality += 1
+//                    }
+//                }
+//            }
+//        }
     }
 }
