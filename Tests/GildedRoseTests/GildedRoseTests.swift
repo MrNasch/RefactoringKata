7b5ed5464be6bd51eb4df0bf41b1ed5eddf56587
MrNasch <nicolaspaschal@hotmail.com>
2020-04-15 18:00:39 +0200

    deleting item and switching to refactoring gidedRose.swift

--- Tests/GildedRoseTests/GildedRoseTests.swift
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -1,29 +1,10 @@
 @testable import GildedRose
 import XCTest
 
-// starting at 17:02
+// starting at 17:02 pause at 18:00
 
 class GildedRoseTests: XCTestCase {
 
-    
-//    Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
-//       Item(name: "Aged Brie", sellIn: 2, quality: 0),
-//       Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
-//       Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
-//       Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
-//       Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
-//       Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
-//       Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
-//       // this conjured item does not work properly yet
-//       Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)]
-    
-//    func testFoo() {
-//        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-//        let app = GildedRose(items: items);
-//        app.updateQuality();
-//        XCTAssertEqual("fixme", app.items[0].name);
-//    }
-    
     // MARK: average item
     
     func testDexterity_DecreaseQuality_NoError() {
